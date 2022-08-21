95cf1f0cec06a98bacf653dbef494de8bf05fc5a
Rayssa Asevedo <rayxaman@hotmail.com>
2022-08-21 20:35:22 -0300

    Update and rename calculoretangulo.java to CalculoRetangulo.java

--- CalculoRetangulo.java
+++ CalculoRetangulo.java
@@ -15,22 +15,24 @@ public class calculoretangulo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        String aux= "";
         float base= 0, alt= 0, area= 0, per= 0;
         
-        aux= JOptionPane.showInputDialog ("Indique a base: ");
-        base = Float.parseFloat (aux);
-        
-        aux= JOptionPane.showInputDialog("Indique a altura: ");
-        alt = Float.parseFloat(aux);
+        base = Float.parseFloat (JOptionPane.showInputDialog ("Indique a base: "));
+        alt = Float.parseFloat (JOptionPane.showInputDialog("Indique a altura: "));
         
         area = (base * alt);
         per = (base + alt)*2;
         
         JOptionPane.showMessageDialog(null, "Retangulo: \n" + "A área  é: " + area + ". \n O perímetro é: " + per);
-        //JOptionPane.showMessageDialog(null, "A área do retangulo é: " + area);
-        //JOptionPane.showMessageDialog(null, "O perímetro do retangulo é: " + per);
-     }
-    
+     }    
 }
+        /*
+        aux= JOptionPane.showInputDialog ("Indique a base: ");
+        base = Float.parseFloat (aux);
+        
+        aux= JOptionPane.showInputDialog("Indique a altura: ");
+        alt = Float.parseFloat(aux); 
+        
+        JOptionPane.showMessageDialog(null, "A área do retangulo é: " + area);
+        JOptionPane.showMessageDialog(null, "O perímetro do retangulo é: " + per);
+        */
